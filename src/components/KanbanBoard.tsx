6db1481ac3206a0e8047a3b892faed85b57011a8
Dmitriifd <idimka9@gmail.com>
2023-09-20 17:16:34 +0500

    KanbanBoard button

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,5 +1,35 @@
 const KanbanBoard = () => {
-  return <div>KanbanBoard</div>;
+  return (
+    <div
+      className="
+        m-auto
+        flex
+        min-h-screen
+        w-full
+        items-center
+        overflow-x-auto
+        overflow-y-hidden
+        px-[40px]
+    ">
+      <div className="m-auto flex gap-4">
+        <button
+          className="h-[60px] w-[350px]
+      min-w-[350px]
+      cursor-pointer
+      rounded-lg
+      bg-mainBackgroundColor
+      border-2
+      border-columnBackgroundColor
+      p-4
+      ring-rose-500
+      hover:ring-2
+      flex
+      gap-2">
+          Add Column
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default KanbanBoard;
